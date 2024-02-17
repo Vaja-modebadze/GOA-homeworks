8ed570d81b05423429cbf4e79e9669164b86c9a4
Vaja-modebadze <vmodebad@gmail.com>
2024-02-17 18:03:32 +0400

    commit

---
+++ day5/classwork/classwork.py
@@ -0,0 +1,5 @@
+book_price = 20
+discount = 5
+print(book_price / discount)
+
+# შევქმენი ცვალდები და გავყავი წიგნის ფასი 5-ზე
---
+++ day5/homework/homework5.py
@@ -0,0 +1,39 @@
+# cvladebis declaracia da values minicheba 
+book1 = 10
+book2 = 20
+book3 = 30
+book4 = 40
+book5 = 50
+book6 = 60
+book7 = 70
+book8 = 80
+book9 = 90
+book10 = 100
+
+discount1 = 10
+
+discounted_price1 = book1 - book1 * discount1 / 100
+discounted_price2 = book2 - book2 * discount1 / 100
+discounted_price3 = book3 - book3 * discount1 / 100
+discounted_price4 = book4 - book4 * discount1 / 100
+discounted_price5 = book5 - book5 * discount1 / 100
+
+discount2 = 20
+
+discounted_price6 = book6 - book6 * discount2 / 100
+discounted_price7 = book7 - book7 * discount2 / 100
+discounted_price8 = book8 - book8 * discount2 / 100
+discounted_price9 = book9 - book9 * discount2 / 100
+discounted_price10 = book10 - book10 * discount2 / 100
+
+# dadeclarirebuli cvladebis mier shesrulebuli moqmedeba gamomaq print punqciis sashvalebit
+print(discounted_price1)
+print(discounted_price2)
+print(discounted_price3)
+print(discounted_price4)
+print(discounted_price5)
+print(discounted_price6)
+print(discounted_price7)
+print(discounted_price8)
+print(discounted_price9)
+print(discounted_price10)
