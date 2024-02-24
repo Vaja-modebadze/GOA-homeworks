1b02266d221b546e776143a0b28f9f2927711370
Vaja-modebadze <vmodebad@gmail.com>
2024-02-24 22:39:37 +0400

    commit

---
+++ day7/classwork/classwork.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num1 == num2)
+print(num1 // num2)
+print(num1 / num2)
+print(num1 > num2)
+print(num1 < num2)
---
+++ day7/homework/homework.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num1 == num2)
+print(num1 // num2)
+print(num1 / num2)
+print(num1 > num2)
+print(num1 < num2)
+
