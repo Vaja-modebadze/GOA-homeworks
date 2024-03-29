31b3f5e86b147448b0b8e1ec162bd4746b60b3d2
Vaja-modebadze <vmodebad@gmail.com>
2024-03-29 19:00:29 +0400

    commit

---
+++ day 16/classwork.py
@@ -0,0 +1,20 @@
+cars = [ "lamborghini", "ASTON MARTIN", "Volkswagen", "Toyota", "Bugatti",]
+
+names = ["Mama" , "Deda", "Ronaldo", "Andrew tate", "nika"]
+
+print(cars)
+print(names)
+
+print(cars[1])
+print(cars[4])
+
+name = "Vaja"
+surname = "Modebadze"
+
+print(name[0] + '.' + surname[0])
+
+vehicles = [ "lamborghini", "ASTON MARTIN", "Volkswagen", "Toyota", "Bugatti",]
+
+name1 = "Ronlado"
+print(len(name1) - 1)
+print(len(vehicles))
---
+++ day 16/homework.py
@@ -0,0 +1,26 @@
+#2)
+movies = ["Resident Evil", "Megamind"]
+print(movies)
+
+#3)
+movies = ["Resident Evil", "Megamind"]
+drinks = ["water", "cola"]
+print(movies + drinks)
+
+#4)
+drinks = ["water", "cola"]
+name = "Kratos"
+print(len(drinks))
+print(len(name))
+
+#5)
+drinks = ["water", "cola"]
+i = drinks[0]
+x = drinks[1]
+print(drinks[1 - 0])
+
+#6) 
+numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+i = numbers[0]
+x = numbers[9]
+print(i + x)
