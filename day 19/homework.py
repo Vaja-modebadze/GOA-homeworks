94bd9d3c8b6062cc54a8487d85f988e0c2562e7d
Vaja-modebadze <vmodebad@gmail.com>
2024-04-06 22:25:10 +0400

    commit

---
+++ day 19/homework.py
@@ -0,0 +1,37 @@
+#1)
+list = []
+range1 = range(1,5)
+for i in range1:
+    list.append(range1)
+    list = range1 * (1,5) 
+print(list)
+
+
+# #2)
+# negative_numbers = []
+
+# for i in range(-3,3):
+#     if i * -1 > 0:
+#         negative_numbers.append(i)
+#         print(i)
+
+# #3)
+# list = []
+# for i in range(1,5):
+#     i = (1 + 2 + 3 + 4)
+
+# print(i / 4)
+    
+# #4)
+# list1 = []
+# list2 = []
+# list3 = []
+
+# for i in range(1,5):
+#     list1.append(i)
+# for x in range(5,10):
+#     list2.append(x)
+            
+# list3.append(list1 and list2)
+# print(list3)
+    
