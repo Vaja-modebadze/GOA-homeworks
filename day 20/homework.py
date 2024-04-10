9ec3caac2a723d0113e13c6dfbcf347a57aa69ba
Vaja-modebadze <vmodebad@gmail.com>
2024-04-10 21:44:22 +0400

    commit

---
+++ day 20/homework.py
@@ -0,0 +1,24 @@
+# 1)
+def even_or_odd(number):
+    if number % 2 == 0:
+        return("Even")
+    else:
+        return("Odd")
+# 2)
+def number_to_string(num):
+    return(str(num))
+
+# 5)
+def bool_to_word(boolean): 
+    while boolean == True:
+        return("Yes")
+    else:
+        return("No")
+
+# 7)
+def remove_char(s):
+    return(s[1:-1])
+
+# 10)
+def double_integer(i):
+    return(i * 2)
