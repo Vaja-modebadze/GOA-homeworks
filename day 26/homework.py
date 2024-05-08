2255d3ef50458b239dc857b4c533525cd8b4a2cd
Vaja-modebadze <vmodebad@gmail.com>
2024-05-08 15:37:59 +0400

    commit

---
+++ day 26/homework.py
@@ -0,0 +1,16 @@
+# 8 kyu:
+# 1)
+def twice_as_old(dad_years_old, son_years_old):
+            return abs(dad_years_old - son_years_old * 2)
+# 2)
+def sum_str(a, b):
+    return str(int('0' + a) + int('0' + b))
+# 3)
+def reverseseq(n):
+    return range(n, 0, -1)
+# 4)
+def no_space(x):
+    return "".join(x.split())
+# 5)
+
+# 7 kyu:
