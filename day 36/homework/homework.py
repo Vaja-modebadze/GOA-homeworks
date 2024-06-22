bb0c42942306c8d9682b754773c1fc751b7b75df
Vaja-modebadze <vmodebad@gmail.com>
2024-06-22 23:04:56 +0400

    commit

---
+++ day 36/homework/homework.py
@@ -0,0 +1,36 @@
+# 1. 
+def fillable(stock, merch, n):
+    if stock == n:
+        return True
+    else:
+        return False
+
+def user_contacts(data):
+    {
+    "Grae Drake": 98110,
+    "Bethany Kok": None,
+    "Alex Nussbacher": 94101,
+    "Darrell Silver": 11201,    
+    }
+    if data == 98110:
+        return "Grae Drake"
+    elif data == None:
+        return "Bethany Kok"
+    elif data == 94101:
+        return "Alex Nussbacher"
+    else:
+        return "Darrell Silver"
+
+# 2.
+# 1)
+student = {
+    "name": "lasha",
+    "age": 16,
+    "country": "Georgia"
+}
+# 2)
+result = 0
+for i in result:
+    result += 1
+print(result)
+
