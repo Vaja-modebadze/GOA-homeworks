c78ede291db15945a179e5c76c48f1c6442e9407
Vaja-modebadze <vmodebad@gmail.com>
2024-09-08 10:00:02 +0400

    commit

---
+++ day 63/classwork/cw.js
@@ -0,0 +1,11 @@
+function roundUp(number) {
+    return Math.ceil(number);
+}
+
+console.log(roundUp(5.1))
+
+function roundDown(number) {
+    return Math.floor(number);
+}
+
+console.log(roundDown(5.4))
