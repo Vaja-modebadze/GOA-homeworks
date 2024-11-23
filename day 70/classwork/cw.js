07bb5321b76fc79c68c403b4c6a49227cf5577db
Vaja-modebadze <vomdebad@gmail.com>
2024-11-23 11:18:30 +0400

    commit

---
+++ day 70/classwork/cw.js
@@ -0,0 +1,3 @@
+const lowercaseStrings = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+const uppercaseStrings = lowercaseStrings.map(string => string.toUpperCase());
+console.log(uppercaseStrings);
