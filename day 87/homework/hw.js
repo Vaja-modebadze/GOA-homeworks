e17b6d149fc89c2f3d3edc9ac186cccf85f32631
Vaja-modebadze <vomdebad@gmail.com>
2024-12-11 12:34:08 +0400

    commit

---
+++ day 87/homework/hw.js
@@ -0,0 +1,6 @@
+let studentList = [
+    { name: "Alice", grade: 90 },
+    { name: "Bob", grade: 85 },
+];
+
+let gradeMap = createStudentGradeMap(studentList);
---
+++ day 89/homework/hw.js
@@ -0,0 +1,8 @@
+let studentList = [
+    { name: "Alice", grade: 90 },
+    { name: "Bob", grade: 85 },
+    { name: "Charlie", grade: 92 },
+    { name: "David", grade: 88 }
+];
+
+let gradeMap = createStudentGradeMap(studentList);
