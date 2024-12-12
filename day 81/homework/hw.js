e2dfba5363fe4dcf8d51c2b979a5e8ab8ad9f562
Vaja-modebadze <vomdebad@gmail.com>
2024-12-12 12:22:10 +0400

    commit

--- day 81/homework/hw.js
+++ day 81/homework/hw.js
@@ -1,1 +1,15 @@
-// https://youtu.be/xli_FI7CuzA
+function countOddDigits() {
+    let num = prompt("Enter an integer:");
+    num = num.trim();
+    let oddDigitCount = 0;
+    for (let i = 0; i < num.length; i++) {
+        let digit = parseInt(num[i], 10);
+        if (digit % 2 !== 0) {
+            oddDigitCount++;
+        }
+    alert("The number contains " + oddDigitCount + " odd digits.");
+}
+
+
+    countOddDigits();
+    }
