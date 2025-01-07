2660e907f4830e05f1e22c790eb10d5c77dbdde4
Vaja-modebadze <vomdebad@gmail.com>
2025-01-07 13:25:05 +0400

    commit

---
+++ day 12/classwork/classwork.py
@@ -0,0 +1,7 @@
+num = int(input("Enter your number: "))
+if num == 0:
+    print(0)
+if num >= 0:
+    print("True")
+else :
+    print("False")
---
+++ day 60/homework/hw.js
@@ -0,0 +1,18 @@
+const book1 = {
+    title: "1",
+    pages: 180
+};
+
+const book2 = {
+    title: "2",
+    pages: 328
+};
+
+const book3 = {
+    title: "3",
+    pages: 281
+};
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
